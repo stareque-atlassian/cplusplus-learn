a79f49d344617a582a64ab38bb16fb036a3c24d6
Shehab Tareque <stareque@atlassian.com>
2021-08-17 18:10:50 +1000

    Initial Commit

---
+++ cplusplus/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  cplusplus
+//
+//  Created by Shehab Tareque on 17/8/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
